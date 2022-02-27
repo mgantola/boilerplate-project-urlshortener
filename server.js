85d3b21ab0349452c801977d2d7e75c2581f5969
Matias G. Antola <mantola@directvla.com>
2022-02-26 22:21:15 -0300

    URL Shortener Microservice Solution :D

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const dns=require("dns");
+const url=require('url');
+const bodyParser=require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -18,6 +24,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+//URL Shortener Microservice Solution :D
+const short_url=Math.floor((Math.random()*50)+1);
+var inc_url="";
+app.post("/api/shorturl",(req,res)=>{
+   inc_url=req.body.url;
+  var furl=url.parse(inc_url);
+  console.log( url.parse(inc_url));
+   console.log(furl.hostname);
+  dns.lookup(furl.hostname,(err,add,fam)=>{
+    if(err){
+      return res.json({error:"invalid url"});
+    }
+    else if(furl.protocol==null||furl.host==null){
+     return res.json({
+        error:"invalid url",
+      });
+    }
+    else{
+     return res.json({
+        original_url:inc_url,
+        short_url:short_url
+      })
+    }
+  })
+});
+
+app.get(`/api/shorturl/${short_url}`,(req,res)=>{
+  console.log(`/api/shorturl/${short_url}`);
+  res.redirect(inc_url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
